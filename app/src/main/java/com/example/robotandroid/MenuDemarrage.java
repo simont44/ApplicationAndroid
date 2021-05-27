ef63e2af020ebf57dee7e76d9a7b082044d726a7
Ameiko <orianegamba1@gmail.com>
2021-05-27 19:38:31 +0200

    Menu Demarrage Opérationnel ! Avec transition vers les gammes

--- app/src/main/java/com/example/robotandroid/MenuDemarrage.java
+++ app/src/main/java/com/example/robotandroid/MenuDemarrage.java
@@ -1,12 +1,11 @@
 package com.example.robotandroid;
 
-import androidx.activity.R;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.fragment.NavHostFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MenuDemarrage extends AppCompatActivity {
 
@@ -14,7 +13,18 @@ public class MenuDemarrage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_demarrage);
+
+        Button buttonGamme = findViewById(R.id.buttonGamme);
+        buttonGamme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenMenuGamme();
+            }
+        });
     }
-
-
+    public void OpenMenuGamme()
+    {
+       Intent menu = new Intent(this, MenuGamme.class);
+       startActivity(menu);
+    }
 }
--- app/src/main/java/com/example/robotandroid/MenuGamme.java
+++ app/src/main/java/com/example/robotandroid/MenuGamme.java
@@ -1,7 +1,13 @@
 package com.example.robotandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
+import
+        androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuGamme extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MenuGamme extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_gamme);
+
+        Button buttonMenu = findViewById(R.id.buttonMenu);
+        buttonMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RetourMenu();
+            }
+        });
+    }
+    public void RetourMenu()
+    {
+        Intent menu = new Intent(this, MenuDemarrage.class);
+        startActivity(menu);
     }
 }
+
