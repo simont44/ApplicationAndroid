401fffc33d1f71d7a1a997ccb4d56e421f178597
Ameiko <orianegamba1@gmail.com>
2021-05-27 18:51:45 +0200

    tentative ecran demarrage : spoil ça marche pas

---
+++ app/src/main/java/com/example/robotandroid/MenuDemarrage.java
@@ -0,0 +1,20 @@
+package com.example.robotandroid;
+
+import androidx.activity.R;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuDemarrage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_demarrage);
+    }
+
+
+}
