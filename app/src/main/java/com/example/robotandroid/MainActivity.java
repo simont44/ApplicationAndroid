90f64abd799348e7cf8c2c46a501140ac92afdef
simont44 <simonthivetveille@gmail.com>
2021-05-29 10:37:57 +0200

    Ajout interface connexion ajout fichier data xml

--- app/src/main/java/com/example/robotandroid/MainActivity.java
+++ app/src/main/java/com/example/robotandroid/MainActivity.java
@@ -3,21 +3,52 @@ package com.example.robotandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText login;
-    private EditText password;
-    private Button connexion;
+    private EditText loginEditText;
+    private EditText passwordEditText;
+    private Button connexionButton;
+    private Boolean loginVerif, passwordVerif;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        login=(EditText) findViewById(R.id.connexion_login_input);
-        password=(EditText) findViewById(R.id.connexion_password_input);
-        connexion=(Button) findViewById(R.id.connexion_submit_btn);
+        loginEditText=(EditText) findViewById(R.id.connexion_login_input);
+        passwordEditText=(EditText) findViewById(R.id.connexion_password_input);
+        connexionButton=(Button) findViewById(R.id.connexion_submit_btn);
+        connexionButton.setEnabled(false);
+
+        loginEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        connexionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
 
 
     }
