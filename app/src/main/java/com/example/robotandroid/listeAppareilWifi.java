6692d3773bd46d0a18354dc9a874308d83f2d901
simont44 <simonthivetveille@gmail.com>
2021-06-01 20:21:26 +0200

    interface de visualisation des périphériques wifi

--- app/src/main/java/com/example/robotandroid/listeAppareilWifi.java
+++ app/src/main/java/com/example/robotandroid/listeAppareilWifi.java
@@ -3,12 +3,32 @@ package com.example.robotandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 public class listeAppareilWifi extends AppCompatActivity {
 
+    private Spinner listeAppareilWifi;
+    final Scanner sc = new Scanner(System.in);//pour lire à partir du clavier
+    private InetAddress adresseIP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_appareil_wifi);
     }
+
 }
