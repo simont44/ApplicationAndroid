1e95d31fe868e80333885a433653bd200ba8ffed
simont44 <simonthivetveille@gmail.com>
2021-06-01 20:21:44 +0200

    Modification de la classe utilisateur (pas sur de moi la)

--- app/src/main/java/com/example/robotandroid/Utilisateur.java
+++ app/src/main/java/com/example/robotandroid/Utilisateur.java
@@ -1,14 +1,38 @@
 package com.example.robotandroid;
 
-public class Utilisateur {
+import java.io.Serializable;
+
+public class Utilisateur implements Serializable {
     String nom;
     boolean admin;
+    String login;
+    String mdp;
 
 
     public Utilisateur(String n, boolean a)
     {
         this.nom = n;
         this.admin = a;
+        this.login="";
+        this.mdp="";
     }
 
+    public void setMdp(String pwd)
+    {
+        this.mdp=pwd;
+    }
+
+
+    public void setLogin(String login)
+    {
+        this.login=login;
+    }
+
+    public String getLogin() {
+        return this.login;
+    }
+
+    public String getMdp() {
+        return this.login;
+    }
 }
